4e976415d5643e0d06f19cea68768e2b31e221d3
Abel Marceau Quentin <quentin.abel-marceau@epita.fr>
2021-09-17 20:04:02 +0200

    Update machine learning image, need to baggare with strings and path

--- NeuralNetwork/main.c
+++ NeuralNetwork/main.c
@@ -31,8 +31,14 @@ static void printWeights(Network *network)
 // launch pixel value in the inputs array, and define expected digit
 // Consider that the image is already in grayscale
 static void createData(char* path, double inputs[28 * 28], double expected[9])
-{
-    SDL_Surface* surface = load_image(path);
+{   
+    char actualpath [PATH_MAX+1];
+    char *ptr;
+    
+    ptr = realpath(path, actualpath);
+    
+    printf("File : %s\n", ptr);    
+    SDL_Surface* surface = load_image(ptr);
 
     //printf("W : %u, H : %u\n", surface->w, surface->h);
 
@@ -43,44 +49,34 @@ static void createData(char* path, double inputs[28 * 28], double expected[9])
             Uint32 pixel = get_pixel(surface, i, j);
             SDL_GetRGB(pixel, surface->format, &rgb.r, &rgb.g, &rgb.b);
             
-            if ((rgb.r + rgb.g + rgb.b) / 3 >= 127){
-                inputs[i * j] = 0.0;
-            }
-            else{
-                inputs[i * j] = 1.0;
-            }
+            inputs[i * j] = (double) (1.0 - ((double) rgb.r / 255.0));
+
             //printf("Input : %f\n", inputs[i * j]);
         }
     }
+    
+    // printf("Path : %s\n", path);
+	// char* delim = "_";
+	// char* s = strtok(path, delim);
 
-	char delim[] = "_";
-	char *s = strtok(path, delim);
+	// while(s != NULL){
+	// 	//printf("'%s'\n", s);
+	// 	s = strtok(NULL, delim);
 
-	while(s != NULL){
-		//printf("'%s'\n", s);
-		s = strtok(NULL, delim);
-
-        // format : 5.jpg
-        if (strstr(s, ".jpg") != NULL) {
-            break;
-        }
-	}  
-
-    uintmax_t num = strtoumax(s, NULL, 10);
+    //     // format : 5.jpg
+    //     if (strstr(s, ".jpg") != NULL){
+    //         break;
+    //     }
+	// }
+    char* p = &path[10];
+    uintmax_t num = strtoumax(p, NULL, 10);
     if (num == UINTMAX_MAX && errno == ERANGE){
         errx(1, "Cannot convert file name to expected value");
     }
 
-    //printf("Expected\n");
     // Init expected value
     for (unsigned int i = 0; i < 9; i++){
-        if (i + 1 == num){
-            expected[i] = 1.0;
-        }
-        else{
-            expected[i] = 0.0;
-        }
-        //printf(" %f", expected[i]);
+        expected[i] = i + 1 == num ? 1.0 : 0.0;
     }
     
     SDL_FreeSurface(surface);
@@ -91,18 +87,17 @@ int main(int argc, char **argv)
     if (argc != 2){
         return 1;
     }
+    unsigned int nbImages = 4;
 
-    double input[28 * 28];
-    double expected[9];
+    double input[nbImages][28 * 28];
+    double expected[nbImages][9];
     
     unsigned int nbInputs = 28 * 28;
     unsigned int nbOutputs = 9;
     unsigned int nbHiddenLayers = 1;
     unsigned int nbNodesPerHidden = 5;
 
-    unsigned int nbImages = 4;
-
-    unsigned int epoch = 1000;
+    unsigned int epoch = 10;
 
     char* inputs[nbImages];
     char* directory = argv[1];
@@ -117,61 +112,66 @@ int main(int argc, char **argv)
     unsigned int index = 0;
     while ((in_file = readdir(FD))) {
         // Check that we don't have the parent directory
-        if (!strcmp (in_file->d_name, "."))
+        if (!strcmp(in_file->d_name, "."))
             continue;
-        if (!strcmp (in_file->d_name, ".."))    
+        if (!strcmp(in_file->d_name, ".."))    
             continue;
 
         char* filename = in_file->d_name;
         printf("\nFile : %s\n", filename);
-        strcpy(directory, "Images");
-        printf("Directory before cat : %s\n", directory);
-        strcat(directory, "/");
-        strcat(directory, filename);
-        inputs[index] = directory;
+        inputs[index] = filename;
         index++;
     }
     closedir(FD);
-    
-    printf("Creating network\n");
 
-    Network n = newNetwork(nbInputs, nbNodesPerHidden, nbHiddenLayers, nbOutputs);
-    Network* network = &n;
-
-    printf("Initing network\n");
-
-    initNetwork(network);
-
-    for (unsigned int i = 0; i <= epoch; i++){
-        if (i % 1 == 0){  
-            printf("###### Epoch : %d ######\n", i);
-        }
-        for (unsigned int j = 0; j < nbImages; j++){ 
-
-            createData(inputs[j], input, expected);
-
-            frontPropagationNetwork(network, input);
-            
-            //printf("Back propagating\n");
-            backPropagation(network, expected);
-            gradientDescent(network);
-
-            // Print expected
-            for (unsigned int l = 0; l < 9; l++){
-                if (expected[l] == 1.0){
-                    printf("\nInput : image of a %u\n", l + 1);
-                    break;
-                }
-            }
-            for (unsigned int k = 0; k < nbOutputs; k++){
-                printf("Output : %f, expected : %u\n", network->layers[nbHiddenLayers + 1].neurons[k].value, (unsigned int) expected[k]);
-            }
-        }
+    for (unsigned int i = 0; i < nbImages; i++){
+        char directory[50];
+        strcpy(directory, "Images/");
+        strcat(directory, inputs[i]);
+        inputs[i] = directory;
+        printf("%s\n", directory);
+        createData(directory, input[i], expected[i]);
     }
 
-    printWeights(network);
+    // printf("Creating network\n");
 
-    freeNetwork(network);
+    // Network n = newNetwork(nbInputs, nbNodesPerHidden, nbHiddenLayers, nbOutputs);
+    // Network* network = &n;
+
+    // printf("Initing network\n");
+
+    // initNetwork(network);
+
+    // for (unsigned int i = 0; i <= epoch; i++){
+    //     if (i % 1 == 0){  
+    //         printf("###### Epoch : %d ######\n", i);
+    //     }
+    //     for (unsigned int j = 0; j < nbImages; j++){ 
+
+    //         printf("File : %s\n", inputs[j]);
+
+    //         frontPropagationNetwork(network, input[j]);
+            
+    //         //printf("Back propagating\n");
+    //         backPropagation(network, expected[j]);
+    //         gradientDescent(network);
+
+    //         // Print expected
+    //         for (unsigned int l = 0; l < 9; l++){
+    //             if (expected[j][l] == 1.0){
+    //                 printf("\nInput : image of a %u\n", l + 1);
+    //                 break;
+    //             }
+    //         }
+    //         for (unsigned int k = 0; k < nbOutputs; k++){
+    //             printf("Output : %f, expected : %u\n", network->layers[nbHiddenLayers + 1].neurons[k].value, (unsigned int) expected[j][k]);
+    //         }
+    //     }
+    // }
+
+    // printWeights(network);
+
+    // freeNetwork(network);
     
     return 0;
 }
