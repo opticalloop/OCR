90a6a5e387967b75e9ae8b98bcc8972c86590ad1
Abel Marceau Quentin <quentin.abel-marceau@epita.fr>
2021-11-15 10:41:54 +0100

    Fixed network training

--- src/NeuralNetwork/main.c
+++ src/NeuralNetwork/main.c
@@ -1,21 +1,25 @@
 #include "NeuralNetwork/training.h"
 #include <SDL/SDL.h>
+#include "Imagery/Utils/image.h"
+#include "Imagery/Color_Treatment/blackandwhite.h"
+#include "Imagery/Detection/reverse_color.h"
 
 int main(void)
 {
-    train(10, 1, 16, 1, "", "w.data");
+    // generateDataFile();
+    train(100, 2, 32, 1, "", "w.data");
 
     // Network n;
     // n.sizeInput = NBINPUTS;
     // n.sizeOutput = NBOUTPUTS;
     // Network *network = &n;
-    // launchWeights(network, "w.data", 1);
+    // launchWeights(network, "w3.data", 1);
     
-    // SDL_Surface *surface = load_image("Digits-Only/digit_0_9.jpg");
+    // SDL_Surface *surface = load_image("9.jpg");
     
     // int res = getNetworkOutput(network, surface, 1);
     // printf("Input : 9\n");
     // printf("Ouput : %d\n", res);
     // freeNetwork(network);
-    // SDL_FreeSurface(surface);
+    // SDL_FreeSurface(surface);    
 }
