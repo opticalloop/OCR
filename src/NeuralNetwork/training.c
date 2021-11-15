c553c5a8ac5d6ca48b022b8848057aa8c4cd638d
Abel Marceau Quentin <quentin.abel-marceau@epita.fr>
2021-11-15 09:42:19 +0100

    Corrected zero expected array

--- src/NeuralNetwork/training.c
+++ src/NeuralNetwork/training.c
@@ -206,7 +206,7 @@ void train(const unsigned int epoch, const unsigned int nbHiddenLayers,
 
     // Init 0 expectation
     int zero_intput[NBINPUTS] = {0};
-    double zero_expected[NBOUTPUTS] = {0.0};
+    double zero_expected[NBOUTPUTS] = {1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
 
     // Open file where data is
     FILE *file;
